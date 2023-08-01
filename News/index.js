292bbbe853db54a50a59c6650188015f88b3062c
Saurabh Raj <saurraj@deloitte.com>
2023-08-01 16:50:18 +0530

    News Website updated with nav bar with category

--- News/index.js
+++ News/index.js
@@ -14,37 +14,56 @@ app.set("views", path.join(__dirname + "/views"));
 const axios = require("axios");
 
 const apiKey = "0175fca54aed4d4dad4df436ca33b072";
-const apiUrl = "http://newsapi.org/v2/everything";
-const q = "india"; // Change this to your desired country code
-const pageSize = 5; // Number of news articles to fetch
-
+const apiUrl = "http://newsapi.org/v2/top-headlines";
+var category = "general"; // Change this to your desired country code
+const pageSize = 12; // Number of news articles to fetch
+const country = "in";
+const language = "en";
 const requestOptions = {
   params: {
-    q,
+    country,
+    category,
     pageSize,
     apiKey,
+    language,
   },
 };
 var articles = [];
-axios
-  .get(apiUrl, requestOptions)
-  .then((response) => {
-    articles = response.data.articles;
-  })
-  .catch((error) => {
-    console.error("Error fetching news:", error.message);
-  });
+
 function formatDate(dateString) {
   const date = new Date(dateString);
   return date.toLocaleDateString(); // Customize this formatting as needed
 }
 
-app.get("/news", async (req, res) => {
+app.get("/", async (req, res) => {
   try {
+    const response = await axios.get(apiUrl, requestOptions);
+    const articles = response.data.articles;
     res.render("News", { articles, formatDate });
   } catch (error) {
     console.error("Error fetching employee data:", error);
     res.status(500).send("Error fetching employee data");
+  }
+});
+
+app.get("/news/:category", async (req, res) => {
+  try {
+    const selectedCategory = req.params.category;
+    const requestOptions = {
+      params: {
+        category: selectedCategory,
+        pageSize,
+        apiKey,
+        language,
+        country,
+      },
+    };
+    const response = await axios.get(apiUrl, requestOptions);
+    const articles = response.data.articles;
+    res.render("News", { articles, formatDate });
+  } catch (error) {
+    console.error("Error fetching news:", error.message);
+    res.status(500).send("Error fetching news");
   }
 });
 
